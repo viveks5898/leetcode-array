1e03a5ec29eeb2ac6457b4a4db4d91f55994ed13
viveks5898 <singh.vivek5898@gmail.com>
2024-02-04 16:33:21 -0800

    duplicate return true

---
+++ Array/containsDuplicate.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    console.log(nums)
+  
+  };
+  console.log(containsDuplicate([[1,2,3,1]]))
--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("first")
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let obj = {};
+    for (let ele of nums) {
+      if (obj[ele]) {
+        return true; 
+      }
+      obj[ele] = true; 
+    }
+    return false; 
+  };
+  
+  console.log(containsDuplicate([1, 2, 3])); // Should output false
+  console.log(containsDuplicate([1, 2, 2])); // Should output true
+  
